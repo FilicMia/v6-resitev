726f9b858bf4cb5473a66c81df7ed03a103c5c49
Mia Filic <mia.filic@fri.uni-lj.si>
2018-12-22 11:41:51 +0000

    redirect solution with $location - SPA approach

--- app_client/other.controller.js
+++ app_client/other.controller.js
@@ -1,9 +1,15 @@
 (function() {
-function otherCtrl($window){
-        $window.location.href = '/';
+/* You can "stay inside" the single page app when using $location in which case 
+you can choose between $location.path(YOUR_URL); or $location.url(YOUR_URL);. 
+So the basic difference between the 
+2 methods is that $location.url() also affects get parameters 
+whilst $location.path() does not. - source: https://stackoverflow.com/questions/27941876/how-to-redirect-to-another-page-using-angularjs  */  
+
+function otherCtrl($location){
+        $location.path('/');
     }
     
-otherCtrl.$inject = ['$window'];
+otherCtrl.$inject = ['$location'];
 
 /* global angular */
 angular
