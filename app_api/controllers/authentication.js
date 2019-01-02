2cf8c3cc87c2a3bcd05781143ba5ceded6cf0358
Mia Filic <mia.filic@fri.uni-lj.si>
2019-01-02 21:56:57 +0000

    fix 11

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -7,4 +7,63 @@ var JSONcallback = function(res, status, msg) {
   res.json(msg);
 };
 
-//rest TODO
+module.exports.login = function(req, res) {
+    /* request consists of body with login data named usernameField: 'mail',
+    passwordField: 'pass' as defined in `passport.js` */
+    if( !req.body.mail || !req.body.pass){
+        JSONcallback(res, 400, {
+          msg: "All data req."
+        });
+        return;
+    }
+    
+    //when all set, authenticate the user data
+    passport.authenticate('local', //strategy
+        function(error, user, data){
+            // error is error data
+            //user from done function in `passport.js` of type User and 
+            //data from the req
+            if(error){
+                JSONcallback(res, 404, error);
+                return;
+            }
+            if(user){
+                JSONcallback(res, 200, {
+                    token: user.genJWT()
+                });
+                return;
+            } else {
+                //unauthorized error
+                JSONcallback(res, 401, data);
+                return;
+            }
+        }
+    )(req,res);
+};
+
+module.exports.register = function(req, res) {
+    /* request consists of body with login data named usernameField: 'mail',
+    passwordField: 'pass' as defined in `passport.js` */
+    if( !req.body.mail || !req.body.pass || !req.body.name){
+        JSONcallback(res, 400, {
+      msg: "All data req."
+    });
+    }
+    
+    //save the data
+    var user = new User({
+        name: req.body.name,
+        mail: req.body.mail
+    });
+    
+    user.storePassword(req.body.pass);
+    user.save(function(error, user){
+        if(error){
+            JSONcallback(res,400,error);
+        } else {
+            JSONcallback(res,200,{
+                token: user.genJWT()
+            });
+        }
+    });
+};
