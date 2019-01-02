f931cf964f66ab39bef1953e8c7dc28cea2ac13a
Mia Filic <mia.filic@fri.uni-lj.si>
2019-01-02 21:48:47 +0000

    fix 10

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').load();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -11,12 +13,18 @@ var fs = require('fs');
 var combinedCode = uglifyJs.minify({
   'app.js': fs.readFileSync('app_client/app.js', 'utf-8'),
   'commentsData.service.js': fs.readFileSync('app_client/all/services/commentsData.service.js', 'utf-8'),
+  // auth client side
+  'auth.service.js': fs.readFileSync('app_client/all/services/auth.service.js', 'utf-8'),
+  'login.controller.js': fs.readFileSync('app_client/auth/login/login.controller.js', 'utf-8'),
+  'registration.controller.js': fs.readFileSync('app_client/auth/registration/registration.controller.js', 'utf-8'),
+  //
   'comments.controller.js': fs.readFileSync('app_client/comments/comments.controller.js', 'utf-8'),
   'other.controller.js': fs.readFileSync('app_client/other.controller.js', 'utf-8'),
   'commentView.controller.js': fs.readFileSync('app_client/comments/commentViewing/commentView.controller.js', 'utf-8'),
   'commentEdit.controller.js': fs.readFileSync('app_client/comments/commentEdit/commentEdit.controller.js', 'utf-8'),
   'comment.directive.js': fs.readFileSync('app_client/all/directives/comment/comment.directive.js', 'utf-8'),
   'footer.directive.js': fs.readFileSync('app_client/all/directives/footer/footer.directive.js', 'utf-8'),
+  'nav.controller.js': fs.readFileSync('app_client/all/directives/nav/nav.controller.js', 'utf-8'),
   'nav.directive.js': fs.readFileSync('app_client/all/directives/nav/nav.directive.js', 'utf-8')
 });
 
@@ -27,8 +35,11 @@ fs.writeFile('public/angular/comments.min.js', combinedCode.code, function(error
     console.log('Script is in "comments.min.js".');
 });
 
+var passport = require('passport');
+
 //add path to the REST api
 require('./app_api/models/db');
+require('./app_api/configuration/passport');
 
 //var indexRouter = require('./app_server/routes/index');
 var indexApi = require('./app_api/routes/index');
@@ -47,6 +58,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
+
+app.use(passport.initialize());
 
 //app.use('/', indexRouter);
 app.use('/api', indexApi);
