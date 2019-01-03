04bfe8c37524fc4c1d5281edbfc919586e835ec5
Mia Filić <filicmia@gmail.com>
2019-01-03 14:24:41 +0000

    comments.controller.js newComment function

--- app_client/comments/comments.controller.js
+++ app_client/comments/comments.controller.js
@@ -12,6 +12,24 @@ function commentsCtrl(commentsData, $location, $scope) {
       vm.msg = "Error while fetching comments.";
       console.log(response.e);
     });
+    
+    vm.newComment = function(){
+      //name: req.body.name,
+       //   comment: req.body.comment,
+       //   pic: req.body.pic,
+       vm.newcomment.pic = '';
+       commentsData.newComment(vm.newcomment).then(
+          function succes(response){
+            vm.msg = response.data.length > 0 ? "" : "No comments.";
+            vm.data.comments.push(response.data);
+            console.log(vm.data);
+          },
+          function error(response){
+            vm.msg = "Error while fetching comments.";
+            console.log(response.e);
+          });
+    };
+    
     $scope.redirectTo = function(comment){
         //redirectTo
          $location.url('/comments/'+comment._id);
